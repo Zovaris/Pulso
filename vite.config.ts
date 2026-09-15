ec19b49e31f75480f5562f87df78eca5f4da4ebd
Bryan Villafuerte <bryamu.u@gmail.com>
2026-09-15 14:58:03 -0600

    chore: add toolchain

---
+++ vite.config.ts
@@ -0,0 +1,36 @@
+import path from "node:path";
+import process from "node:process";
+import tailwindcss from "@tailwindcss/vite";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+
+const host = process.env.TAURI_DEV_HOST;
+
+export default defineConfig({
+  resolve: {
+    alias: {
+      "@": path.resolve(process.cwd(), "src"),
+    },
+  },
+  plugins: [react(), tailwindcss()],
+  clearScreen: false,
+  build: {
+    sourcemap: false,
+    minify: "esbuild",
+  },
+  server: {
+    port: 1420,
+    strictPort: true,
+    host: host || false,
+    hmr: host
+      ? {
+          protocol: "ws",
+          host,
+          port: 1421,
+        }
+      : undefined,
+    watch: {
+      ignored: ["**/src-tauri/**"],
+    },
+  },
+});
